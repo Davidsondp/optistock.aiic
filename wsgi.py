e6c4732c979806d5f961aeacc3fdbcfffdeb9086
Davidsondp <technologyinte@gmail.com>
2025-08-06 21:01:01 -0300

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,5 +0,0 @@
-from app import app  # Asegúrate que tu app.py define "app = Flask(__name__)"
-
-# Si tu app.py usa create_app(), cambia esta línea por:
-# from app import create_app
-# app = create_app()
